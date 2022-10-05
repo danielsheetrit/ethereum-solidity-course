f78dfe89d3788c145914c27b880689161884d4d1
danieldevgki <daniels@gkigroup.com>
2022-10-05 19:24:18 +0300

    implement testing with mocha

--- compile.js
+++ compile.js
@@ -1,1 +1,13 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+/**
+ * __dirname always point to the root directory "Inbox"
+ * in our case.
+ */
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+const inboxContract = solc.compile(source, 1).contracts[':Inbox']
+
+module.exports = inboxContract;
